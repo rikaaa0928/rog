505aaacb5a2dd559f2e6a571eb426dca73fe7bf6
rikaaa0928 <8528731+rikaaa0928@users.noreply.github.com>
2025-05-30 09:52:17 +0800

    rog v2 remove

--- src/connector/mod.rs
+++ src/connector/mod.rs
@@ -1,11 +1,9 @@
 use crate::connector::grpc::GrpcRunConnector;
 use crate::connector::tcp::TcpRunConnector;
-use crate::connector::rogv2::RogV2Connector;
 use crate::def::{config, RunConnector};
 
 pub(crate) mod tcp;
 pub(crate) mod grpc;
-pub(crate) mod rogv2;
 
 pub async fn create(cfg: &config::Connector) -> std::io::Result<Box<dyn RunConnector>> {
     match cfg.proto.as_str() {
@@ -15,10 +13,6 @@ pub async fn create(cfg: &config::Connector) -> std::io::Result<Box<dyn RunConne
         }
         "grpc" => {
             let res=GrpcRunConnector::new(cfg).await?;
-            Ok(Box::new(res))
-        }
-        "rogv2" => {
-            let res = RogV2Connector::new(cfg).await?;
             Ok(Box::new(res))
         }
         _ => {
--- src/listener/mod.rs
+++ src/listener/mod.rs
@@ -3,7 +3,6 @@ use crate::listener::grpc::GrpcListener;
 use crate::listener::http::HttpRunAcceptor;
 use crate::listener::socks5::SocksRunAcceptor;
 use crate::listener::tcp::TcpRunListener;
-use crate::listener::rogv2::RogV2Listener;
 use crate::object::config::ObjectConfig;
 use std::sync::Arc;
 
@@ -11,7 +10,6 @@ pub(crate) mod grpc;
 pub(crate) mod http;
 pub(crate) mod socks5;
 pub(crate) mod tcp;
-pub(crate) mod rogv2;
 
 pub async fn create(
     cfg: &ObjectConfig,
@@ -36,11 +34,6 @@ pub async fn create(
                 .await?;
             let http = Box::new(HttpRunAcceptor::new(listener, None, None));
             Ok(http)
-        }
-        "rogv2" => {
-            let rogv2 = RogV2Listener::new(cfg.listener.clone()); // router argument removed from call
-            let acc = rogv2.listen(&cfg.listener.endpoint).await?;
-            Ok(acc)
         }
         _ => Err(std::io::Error::new(
             std::io::ErrorKind::InvalidInput,
